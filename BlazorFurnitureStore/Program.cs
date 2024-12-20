7f9a8746a7eca988a08b85039d51e939769907ec
“yingzhang0897” <yinganticpc@gmail.com>
2024-12-19 21:30:04 -0800

    Register the DbContext in Program.cs

--- BlazorFurnitureStore/Program.cs
+++ BlazorFurnitureStore/Program.cs
@@ -8,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
 
 var app = builder.Build();
 
